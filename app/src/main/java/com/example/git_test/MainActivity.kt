f3cc1872672d994d1f0a5baa40b4afe04a3f07a2
serak0310 <serak0310@naver.com>
2022-04-04 18:20:57 +0900

    주석 없애기

--- app/src/main/java/com/example/git_test/MainActivity.kt
+++ app/src/main/java/com/example/git_test/MainActivity.kt
@@ -7,5 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // 머지머지 머지가 머지
+        //기억이 좀 가물가물해졋을텐데
     }
 }
